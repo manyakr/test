aa957e86852ab31ce20a5d0011223c0f03558ca0
manyakr <98389564+manyakr@users.noreply.github.com>
2022-04-29 11:03:08 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum{
+	 public static void main(String[] args)
+	{
+
+		int a = 5;
+		int b = 12;
+		int sum = a + b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(sum);
+	}
+	
+}
